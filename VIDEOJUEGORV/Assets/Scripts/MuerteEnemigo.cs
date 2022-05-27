897aa5858f4b7b1d877b5dedcbca82ad0380c519
Carlos19-Palacios <carlos_a.riascos@uao.edu.co>
2022-05-27 14:52:33 -0500

    VIDEOJUEGORV

---
+++ VIDEOJUEGORV/Assets/Scripts/MuerteEnemigo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuerteEnemigo : MonoBehaviour 
+{
+	public int CurrentHealth = 20; //Se le dice la vida
+	public int damage = 1; //Daño aplicado
+
+	//Pegar al enemigo con la bala, que cuando entre al trigger le haga daño
+	private void OnTriggerEnter(Collider other) 
+	{
+		if (GameObject.FindWithTag("Bullet")) 
+		{
+			DamageEnemy(damage);
+		}
+	}
+	//Cantidad de daño aplicado a la vida, si llega a 0 se destruye el objeto
+	public void DamageEnemy(int damageAmount) 
+	{
+		CurrentHealth -= damageAmount;
+		if (CurrentHealth <= 0) 
+		{
+			Destroy(gameObject);
+		}
+	}
+}
+
---
+++ VIDEOJUEGORV/Assets/Scripts/MuerteJugador.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuerteJugador : MonoBehaviour
+{
+	public int CurrentHealth = 20; //Se le dice la vida
+	public int damage = 1; //Daño aplicado
+
+	//Que el trigger del enemigo dañe al jugador
+	private void OnTriggerEnter(Collider other)
+	{
+		if (GameObject.FindWithTag("Enemy"))
+		{
+			DamagePlayer(damage);
+		}
+	}
+	//Cantidad de daño aplicado a la vida, si llega a 0 se destruye el objeto
+	public void DamagePlayer(int damageAmount)
+	{
+		CurrentHealth -= damageAmount;
+		if (CurrentHealth <= 0)
+		{
+			Destroy(gameObject);
+			ControlEscenas.CambiarEscenaStatic(ControlEscenas.Levels.GameOver);
+		}
+	}
+}
