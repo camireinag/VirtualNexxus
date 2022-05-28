403a3e572fc0a274dd26a0bf40fb6e0d6e1c2305
Carlos19-Palacios <carlos_a.riascos@uao.edu.co>
2022-05-28 03:39:48 -0500

    LastOne

--- VIDEOJUEGORV/Assets/Scripts/Aparicion.cs
+++ VIDEOJUEGORV/Assets/Scripts/Aparicion.cs
@@ -16,7 +16,7 @@ public class Aparicion : MonoBehaviour
 		if (Timer < Time.time)
 		{ //This checks wether real time has caught up to the timer
 			Instantiate(Enemy, transform.position, transform.rotation); //This spawns the emeny
-			Timer = Time.time + 2; // THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE SECOND TIME 
+			Timer = Time.time + 8; // THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE SECOND TIME 
 		}
 	}
 }
--- VIDEOJUEGORV/Assets/Scripts/MuerteJugador.cs
+++ VIDEOJUEGORV/Assets/Scripts/MuerteJugador.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MuerteJugador : MonoBehaviour
 {
-	public int CurrentHealth = 20; //Se le dice la vida
 	public int damage = 1; //Daño aplicado
-
+	public int heal= 1;
+	[SerializeField] int CurrentHealth = 20;//Se le dice la vida
+	[SerializeField] Text vida; //Texto de vida
 	//Que el trigger del enemigo dañe al jugador
 	private void OnTriggerEnter(Collider other)
 	{
@@ -13,15 +15,26 @@ public class MuerteJugador : MonoBehaviour
 		{
 			DamagePlayer(damage);
 		}
+       /* if (GameObject.FindWithTag("Heal")) 
+		{
+			HealPlayer(heal);
+		}*/
 	}
 	//Cantidad de daño aplicado a la vida, si llega a 0 se destruye el objeto
 	public void DamagePlayer(int damageAmount)
 	{
 		CurrentHealth -= damageAmount;
+		vida.text = string.Format("{00}", CurrentHealth);
 		if (CurrentHealth <= 0)
 		{
 			Destroy(gameObject);
 			ControlEscenas.CambiarEscenaStatic(ControlEscenas.Levels.GameOver);
 		}
 	}
+	/*public void HealPlayer(int healAmount) 
+	{
+		CurrentHealth += healAmount;
+		vida.text = string.Format("{00}", CurrentHealth);
+
+	}*/
 }
