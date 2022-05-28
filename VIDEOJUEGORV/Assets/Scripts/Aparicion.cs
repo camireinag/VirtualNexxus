652c08f7ab742caa59ca59b67cd82733a65ffa44
SamirHO <samir.hassan@uao.edu.co>
2022-05-28 10:52:17 -0500

    UltimosCambios

---
+++ VIDEOJUEGORV/Assets/Scripts/Aparicion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aparicion : MonoBehaviour
+{
+	public Transform Enemy;
+	private float Timer;
+	// Use this for initialization
+	void Start()
+	{
+		Timer = Time.time + 2;// THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE FIRST TIME 
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (Timer < Time.time)
+		{ //This checks wether real time has caught up to the timer
+			Instantiate(Enemy, transform.position, transform.rotation); //This spawns the emeny
+			Timer = Time.time + 8; // THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE SECOND TIME 
+		}
+	}
+}
---
+++ VIDEOJUEGORV/Assets/Scripts/Temporizador.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Temporizador : MonoBehaviour 
+{
+	[SerializeField] int min, seg;
+	[SerializeField] Text tiempo;
+	public bool enMarcha;
+	public float restante;
+
+	private void Awake() 
+	{
+	restante = (min *60) + seg;
+	enMarcha = true;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+        if (enMarcha)
+        {
+			restante -= Time.deltaTime;
+			int tempMin = Mathf.FloorToInt(restante / 60);
+			int tempSeg = Mathf.FloorToInt(restante % 60);
+			tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+		}
+		if (restante < 1)
+		{
+			enMarcha = true;
+			ControlEscenas.CambiarEscenaStatic(ControlEscenas.Levels.GameCompleted);
+		}
+	}
+}
