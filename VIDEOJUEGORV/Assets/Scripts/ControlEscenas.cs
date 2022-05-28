9eb222509878b28dfa14345f4e23389788e772f0
Juanpablo514 <juan_pablo.patino@uao.edu.co>
2022-05-28 10:59:13 -0500

    Animación

---
+++ VIDEOJUEGORV/Assets/Scripts/ControlEscenas.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ControlEscenas : MonoBehaviour 
+{
+    public enum Levels 
+    {
+        FirstPart = 0, SecondPart = 1, Menu = 2, Game = 3, GameCompleted = 4, GameOver = 5
+    }
+    public void CambiarEscena(Levels level) 
+    {
+        SceneManager.LoadScene((int)level);
+    }
+    public void CambiarEscena(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+    public static void CambiarEscenaStatic(Levels level) 
+    {
+        SceneManager.LoadScene((int)level);
+    }
+}
